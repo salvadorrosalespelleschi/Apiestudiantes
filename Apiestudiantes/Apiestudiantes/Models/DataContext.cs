dc417db4507e1450c00a5fe5d9aea5256368069f
Salvador Rosales Pelleschi <a2018114271@estudiantes.upsa.edu.bo>
2020-08-29 08:01:15 -0400

    Models

---
+++ Apiestudiantes/Apiestudiantes/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Apiestudiantes.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<Apiestudiantes.Models.Student> Students { get; set; }
+    }
+}
